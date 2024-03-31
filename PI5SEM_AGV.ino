2a1dac1c34b3e6b2c6209d60743ed28d53223691
55Hian <contato.hian@outlook.com>
2024-03-31 01:37:10 -0300

    feat: implementação de atualização por OTA da placa e estilização da pagina de login

---
+++ PI5SEM_AGV.ino
@@ -0,0 +1,245 @@
+/*  Programa para ESP32 antes da atualização OTA */
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <Update.h>
+ 
+/* Constantes - conexão wi-fi e webserver */
+const char* host = "DUP";
+const char* ssid = "CACHORRO"; /* coloque aqui o nome da rede wi-fi que o ESP32 deve se conectar */
+const char* password = "hi12345678"; /* coloque aqui a senha da rede wi-fi que o ESP32 deve se conectar */
+ 
+/* Variáveis globais */
+int contador_ms = 0;
+ 
+/* Webserver para se comunicar via browser com ESP32  */
+WebServer server(80);
+ 
+/* Códigos da página que será aberta no browser 
+   (quando comunicar via browser com o ESP32) 
+   Esta página exigirá um login e senha, de modo que somente 
+   quem tenha estas informações consiga atualizar o firmware
+   do ESP32 de forma OTA */
+const char* loginIndex = 
+
+"<div id='main'>"
+    "<img width='100' height='100' src='https://media.licdn.com/dms/image/C4D0BAQF2nIpNK4FMpQ/company-logo_100_100/0/1651974426008?e=1720051200&v=beta&t=6qMQGTnhTPqcXQm0ChEYKNLXMRANOAwCpnFiDSfIw20' alt='logo Dup'>"
+    "<form name='loginForm'>"
+        "<h1> DUP - Atualizar codigos </h1>"
+
+        "<div class='controleform'>"
+            "<label for='usuario'>Usuario</label>"
+            "<input type='text' id='usuario' name='userid' placeholder='seu nome de usuario'>"
+        "</div>"
+
+        "<div class='controleform'>"
+            "<label for='senha'>Senha</label>"
+            "<input type='Password' id='senha' name='pwd' placeholder='sua senha'>"
+        "</div>"
+
+        "<input id='bot' type='submit' onclick='check(this.form)' value='Identificar'>"
+    "</form>"
+"</div>"
+"<style>"
+
+    "img{"
+    "border-radius: 50px;"
+    "}"
+
+    ":root{"
+        "font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;"
+    "}"
+
+    "h1{"
+        "font-size: 14pt"
+    "}"
+
+    "#main{"
+        "display: flex;"
+        "align-items: center;"
+        "flex-direction: column;"
+        "margin: 100px;"
+    "}"
+
+    ".controleform{"
+        "display: flex;"
+        "flex-direction: column;"
+        "width: 300px;"
+        "height: 75px;"
+    "}"
+
+    ".controleform label{"
+    "font-size: 10pt;"
+    "padding-bottom: 5px;"
+    "}"
+    
+    ".controleform input{"
+        "height: 30px;"
+        "border: solid 1px #807e7e;"
+        "border-radius: 4px;"
+    "}"
+
+    "#bot{"
+        "height: 40px;"
+        "width: 100%;"
+        "border: none;"
+        "border-radius: 8px;"
+        "color: #000000;"
+        "cursor: pointer;"
+        "box-shadow: inset 2px 2px 1px #ac9fa8;"
+        "background-color: #8ef487;"
+        "}"
+
+    "#bot:active {"
+        "box-shadow: inset 1px 1px 2px #ac9fa8;"
+    "}"
+
+    "#bot:hover, #bot:focus {"
+    "background-color: #55e855;"
+    "}"
+
+"</style>"
+
+"<script>"
+    "function check(form)"
+    "{"
+    "if(form.userid.value=='admin' && form.pwd.value=='admin')"
+    "{"
+    "window.open('/serverIndex')"
+    "}"
+    "else"
+    "{"
+    " alert('Login ou senha inválidos')"
+    "}"
+    "}"
+"</script>";
+  
+const char* serverIndex = 
+"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+   "<input type='file' name='update'>"
+        "<input type='submit' value='Update'>"
+    "</form>"
+ "<div id='prg'>Progresso: 0%</div>"
+ "<script>"
+  "$('form').submit(function(e){"
+  "e.preventDefault();"
+  "var form = $('#upload_form')[0];"
+  "var data = new FormData(form);"
+  " $.ajax({"
+  "url: '/update',"
+  "type: 'POST',"
+  "data: data,"
+  "contentType: false,"
+  "processData:false,"
+  "xhr: function() {"
+  "var xhr = new window.XMLHttpRequest();"
+  "xhr.upload.addEventListener('progress', function(evt) {"
+  "if (evt.lengthComputable) {"
+  "var per = evt.loaded / evt.total;"
+  "$('#prg').html('Progresso: ' + Math.round(per*100) + '%');"
+  "}"
+  "}, false);"
+  "return xhr;"
+  "},"
+  "success:function(d, s) {"
+  "console.log('Sucesso!')"
+ "},"
+ "error: function (a, b, c) {"
+ "}"
+ "});"
+ "});"
+ "</script>";
+ 
+void setup(void) 
+{
+    Serial.begin(115200);
+ 
+    /* Conecta-se a rede wi-fi */
+    WiFi.begin(ssid, password);
+    Serial.println("");
+ 
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        delay(500);
+        Serial.print(".");
+    }
+     
+    Serial.println("");
+    Serial.print("Conectado a rede wi-fi ");
+    Serial.println(ssid);
+    Serial.print("IP obtido: ");
+    Serial.println(WiFi.localIP());
+ 
+    /* Usa MDNS para resolver o DNS */
+    if (!MDNS.begin(host)) 
+    { 
+        //http://esp32.local
+        Serial.println("Erro ao configurar mDNS. O ESP32 vai reiniciar em 1s...");
+        delay(1000);
+        ESP.restart();        
+    }
+   
+    Serial.println("mDNS configurado e inicializado;");
+   
+    /* Configfura as páginas de login e upload de firmware OTA */
+    server.on("/", HTTP_GET, []() 
+    {
+        server.sendHeader("Connection", "close");
+        server.send(200, "text/html", (loginIndex));
+    });
+     
+    server.on("/serverIndex", HTTP_GET, []() 
+    {
+        server.sendHeader("Connection", "close");
+        server.send(200, "text/html", serverIndex);
+    });
+   
+    /* Define tratamentos do update de firmware OTA */
+    server.on("/update", HTTP_POST, []() 
+    {
+        server.sendHeader("Connection", "close");
+        server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+        ESP.restart();
+    }, []() {
+        HTTPUpload& upload = server.upload();
+         
+        if (upload.status == UPLOAD_FILE_START) 
+        {
+            /* Inicio do upload de firmware OTA */
+            Serial.printf("Update: %s\n", upload.filename.c_str());
+            if (!Update.begin(UPDATE_SIZE_UNKNOWN)) 
+                Update.printError(Serial);
+        } 
+        else if (upload.status == UPLOAD_FILE_WRITE) 
+        {
+            /* Escrevendo firmware enviado na flash do ESP32 */
+            if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) 
+                Update.printError(Serial);      
+        } 
+        else if (upload.status == UPLOAD_FILE_END) 
+        {
+            /* Final de upload */
+            if (Update.end(true))             
+                Serial.printf("Sucesso no update de firmware: %u\nReiniciando ESP32...\n", upload.totalSize);
+            else
+                Update.printError(Serial);
+        }   
+    });
+    server.begin();
+}
+ 
+void loop() 
+{
+    server.handleClient();
+    delay(1);
+ 
+    contador_ms++;
+ 
+    if (contador_ms >= 1000)
+    {    
+        Serial.println("programa do hian que subiu por vscode");
+        contador_ms = 0;
+    }
+}
