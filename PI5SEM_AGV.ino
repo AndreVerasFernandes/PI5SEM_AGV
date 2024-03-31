91c2aadbc838b5da6ee8586bf8001f473e3e22a2
55Hian <contato.hian@outlook.com>
2024-03-31 14:26:29 -0300

    fix: Remoção dos arquivos HTML da pagina de login e update desse arquivo fonte e chamada de novas funções para os novos arquivos fonte que contem os dados HTML

--- PI5SEM_AGV.ino
+++ PI5SEM_AGV.ino
@@ -4,6 +4,9 @@
 #include <WebServer.h>
 #include <ESPmDNS.h>
 #include <Update.h>
+
+const char* loginPage();
+const char* updatePage();
  
 /* Constantes - conexão wi-fi e webserver */
 const char* host = "DUP";
@@ -19,138 +22,11 @@ WebServer server(80);
 /* Códigos da página que será aberta no browser 
    (quando comunicar via browser com o ESP32) 
    Esta página exigirá um login e senha, de modo que somente 
-   quem tenha estas informações consiga atualizar o firmware
+   quem tenha estas informações consiga atualizar o firmware 
    do ESP32 de forma OTA */
-const char* loginIndex = 
+const char* loginIndex = loginPage(); 
+const char* serverIndex = updatePage();
 
-"<div id='main'>"
-    "<img width='100' height='100' src='https://media.licdn.com/dms/image/C4D0BAQF2nIpNK4FMpQ/company-logo_100_100/0/1651974426008?e=1720051200&v=beta&t=6qMQGTnhTPqcXQm0ChEYKNLXMRANOAwCpnFiDSfIw20' alt='logo Dup'>"
-    "<form name='loginForm'>"
-        "<h1> DUP - Atualizar codigos </h1>"
-
-        "<div class='controleform'>"
-            "<label for='usuario'>Usuario</label>"
-            "<input type='text' id='usuario' name='userid' placeholder='seu nome de usuario'>"
-        "</div>"
-
-        "<div class='controleform'>"
-            "<label for='senha'>Senha</label>"
-            "<input type='Password' id='senha' name='pwd' placeholder='sua senha'>"
-        "</div>"
-
-        "<input id='bot' type='submit' onclick='check(this.form)' value='Identificar'>"
-    "</form>"
-"</div>"
-"<style>"
-
-    "img{"
-    "border-radius: 50px;"
-    "}"
-
-    ":root{"
-        "font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;"
-    "}"
-
-    "h1{"
-        "font-size: 14pt"
-    "}"
-
-    "#main{"
-        "display: flex;"
-        "align-items: center;"
-        "flex-direction: column;"
-        "margin: 100px;"
-    "}"
-
-    ".controleform{"
-        "display: flex;"
-        "flex-direction: column;"
-        "width: 300px;"
-        "height: 75px;"
-    "}"
-
-    ".controleform label{"
-    "font-size: 10pt;"
-    "padding-bottom: 5px;"
-    "}"
-    
-    ".controleform input{"
-        "height: 30px;"
-        "border: solid 1px #807e7e;"
-        "border-radius: 4px;"
-    "}"
-
-    "#bot{"
-        "height: 40px;"
-        "width: 100%;"
-        "border: none;"
-        "border-radius: 8px;"
-        "color: #000000;"
-        "cursor: pointer;"
-        "box-shadow: inset 2px 2px 1px #ac9fa8;"
-        "background-color: #8ef487;"
-        "}"
-
-    "#bot:active {"
-        "box-shadow: inset 1px 1px 2px #ac9fa8;"
-    "}"
-
-    "#bot:hover, #bot:focus {"
-    "background-color: #55e855;"
-    "}"
-
-"</style>"
-
-"<script>"
-    "function check(form)"
-    "{"
-    "if(form.userid.value=='admin' && form.pwd.value=='admin')"
-    "{"
-    "window.open('/serverIndex')"
-    "}"
-    "else"
-    "{"
-    " alert('Login ou senha inválidos')"
-    "}"
-    "}"
-"</script>";
-  
-const char* serverIndex = 
-"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
-"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
-   "<input type='file' name='update'>"
-        "<input type='submit' value='Update'>"
-    "</form>"
- "<div id='prg'>Progresso: 0%</div>"
- "<script>"
-  "$('form').submit(function(e){"
-  "e.preventDefault();"
-  "var form = $('#upload_form')[0];"
-  "var data = new FormData(form);"
-  " $.ajax({"
-  "url: '/update',"
-  "type: 'POST',"
-  "data: data,"
-  "contentType: false,"
-  "processData:false,"
-  "xhr: function() {"
-  "var xhr = new window.XMLHttpRequest();"
-  "xhr.upload.addEventListener('progress', function(evt) {"
-  "if (evt.lengthComputable) {"
-  "var per = evt.loaded / evt.total;"
-  "$('#prg').html('Progresso: ' + Math.round(per*100) + '%');"
-  "}"
-  "}, false);"
-  "return xhr;"
-  "},"
-  "success:function(d, s) {"
-  "console.log('Sucesso!')"
- "},"
- "error: function (a, b, c) {"
- "}"
- "});"
- "});"
- "</script>";
  
 void setup(void) 
 {
@@ -239,7 +115,8 @@ void loop()
  
     if (contador_ms >= 1000)
     {    
-        Serial.println("programa do hian que subiu por vscode");
+        Serial.print("Servidor online, acesse pelo IP: ");
+        Serial.println(WiFi.localIP());
         contador_ms = 0;
     }
 }
